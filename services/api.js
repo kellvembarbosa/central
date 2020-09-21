d8a86326a459bb342b9b2b27ba1b210bae40762b
Kellvem Barbosa <kellvembarbosa@icloud.com>
2020-09-21 16:50:39 +0100

    test

---
+++ services/api.js
@@ -0,0 +1,6 @@
+// In a node.js environment
+const Parse = require('parse/node')
+
+Parse.initialize("ey43R4j6uk5MVNdNLUTFBhGgcD6NGKcgHKmb7Eul", "56A1znwYSNjP0Tz0UrDgee4OdutbEatF02i71CmJ");
+Parse.serverURL = 'https://parseapi.back4app.com/'
+
